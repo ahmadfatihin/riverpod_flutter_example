2647a40a06227d2362e1196d290cc398dc6c6d00
Prima Interaktif - Developer <ahmdfatihin17@gmail.com>
2023-01-05 11:03:23 +0700

    feat: implement state notifier riverpod

--- lib/module/form/form_name_page.dart
+++ lib/module/form/form_name_page.dart
@@ -1,56 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_demo/module/form/providers/form_provider.dart';
 
-class FormNamePage extends StatelessWidget {
+class FormNamePage extends ConsumerWidget {
   FormNamePage({Key? key}) : super(key: key);
 
   final TextEditingController controller = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    var form = ref.watch(formProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Person Provider Demo'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 45,
-              width: 300,
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(color: Colors.blue)),
-              child: TextField(
-                controller: controller,
-                decoration: const InputDecoration(
-                    border: InputBorder.none, hintText: 'New Name'),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 45,
+                width: 300,
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.blue)),
+                child: TextField(
+                  controller: controller,
+                  decoration: const InputDecoration(
+                      border: InputBorder.none, hintText: 'New Name'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'name',
-              style: TextStyle(fontSize: 30),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              '0',
-              style: TextStyle(fontSize: 30),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(onPressed: () {}, child: const Text('Change Name')),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(onPressed: () {}, child: const Text('Age')),
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                form.name,
+                style: const TextStyle(fontSize: 30),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                '${form.age}',
+                style: const TextStyle(fontSize: 30),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(50),
+                  ),
+                  onPressed: () {
+                    ref.read(formProvider.notifier).changeName(controller.text);
+                  },
+                  child: const Text('Change Name')),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(50),
+                  ),
+                  onPressed: () {
+                    ref.read(formProvider.notifier).changeAge();
+                  },
+                  child: const Text('Increase Age')),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/module/form/providers/form_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_demo/module/form/models/form_name_model.dart';
+
+class FormNotifier extends StateNotifier<FormName> {
+  FormNotifier() : super(const FormName(name: 'Name not filled', age: 0));
+
+  void changeName(String name) => state = state.copyWith(name: name);
+  void changeAge() => state = state.copyWith(age: state.age + 1);
+}
+
+final formProvider = StateNotifierProvider<FormNotifier, FormName>((ref) {
+  return FormNotifier();
+});
